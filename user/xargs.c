e6e54e106d6656a3573e275a7d6e43ec410d62ed
PrajwalM2212 <prajwalmmath@gmail.com>
2019-10-09 21:01:22 +0530

    add xargs

--- user/xargs.c
+++ user/xargs.c
@@ -7,37 +7,24 @@
 
 
 int main(int argc, char **argv) {
-    //printf("%s %s\n", *(argv+2), *(argv+3));
-    //exec(argv[1], argv+1);
-    int count = 0;
-    while(count < 10) {
-        count += 1;
+    char line;
+    while(read(0,&line,1)) {
         int pid = fork();
         if (pid == 0) {
-            char line;
-            int i = 0;
+            int i = 1;
             int n;
-            //printf("Child\n");
+            printf("Child\n");
 
             while ((n = read(0, &line + i, 1)) == 1 && *(&line + i) != '\n') {
                 i += 1;
             }
             *(&line + i) = 0;
-            //i = 0;
-            //while (*(argv + i) != 0) {
-            //printf("%s\n", *(argv+i));
-            //    i += 1;
-            //}
-            //argv[i] = &line;
-            //printf("%s\n", &line);
             *(argv + argc) = &line;
             exec(argv[1], argv + 1);
             exit(0);
         } else {
             int estatus;
-            //while(1) {
             wait(&estatus);
-            //}
             *(argv + argc) = 0;
             printf("%d \n", estatus);
             printf("Completed %d\n");
